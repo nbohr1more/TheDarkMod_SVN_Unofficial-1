a23e165570c55809db2388072fd986e6179d9d4f
nbohr1more <nbohr314@yahoo.com>
2014-11-29 01:17:45 -0500

    RGTC changes

--- renderer/Image_load.cpp
+++ renderer/Image_load.cpp
@@ -80,6 +80,8 @@ int idImage::BitsForInternalFormat( int internalFormat ) const {
 		return 8;
 	case GL_COMPRESSED_LUMINANCE_ALPHA_LATC2_EXT:
 		return 8;
+	case GL_COMPRESSED_RED_GREEN_RGTC2_EXT: // nbohr1more: RGTC normal map compression
+		return 8;
 	case GL_RGBA4:
 		return 16;
 	case GL_RGB5:
@@ -258,6 +260,9 @@ GLenum idImage::SelectInternalFormat( const byte **dataPtrs, int numDataPtrs, in
 		} else if ( globalImages->image_useCompression.GetBool() && globalImages->image_useNormalCompression.GetInteger() == 2 && glConfig.textureCompressionAvailable ) {
 			// image_useNormalCompression == 2 uses rxgb format which produces really good quality for medium settings
 			return GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+		// } else if( globalImages->image_useCompression.GetBool() && globalImages->image_useNormalCompression.GetInteger() == 7 && glConfig.textureCompressionAvailable ) {
+		// image_useNormalCompression == 7 uses rgtc compression the best available format
+		// return GL_COMPRESSED_RED_GREEN_RGTC2_EXT;
 		} else {
 			// we always need the alpha channel for bump maps for swizzling
 			return GL_RGBA8; 
@@ -1110,6 +1115,9 @@ void idImage::WritePrecompressedImage() {
 				case GL_COMPRESSED_RGBA_S3TC_DXT5_EXT:
 					format = "DXT5";
 					break;
+				case GL_COMPRESSED_RED_GREEN_RGTC2_EXT: // nbohr1more RGTC normal map compression
+					format = "RGTC";
+					break;
 			}
 		}
 		globalImages->AddDDSCommand( va( "z:/d3xp/compressonator/thecompressonator -convert \"%s\" \"%s\" %s -mipmaps\n", inFile.c_str(), outFile.c_str(), format.c_str() ) );
@@ -1165,6 +1173,9 @@ void idImage::WritePrecompressedImage() {
 			break;
 		case GL_COMPRESSED_RGBA_S3TC_DXT5_EXT:
 			header.ddspf.dwFourCC = DDS_MAKEFOURCC('D','X','T','5');
+			break;
+		case GL_COMPRESSED_RED_GREEN_RGTC2_EXT;
+			header.ddspf.dwFourCC = DDS_MAKEFOURCC( 'R', 'G', 'T', 'C' ):
 			break;
 		}
 	} else {
@@ -1465,6 +1476,9 @@ void idImage::UploadPrecompressedImage( byte *data, int len ) {
 			break;
 		case DDS_MAKEFOURCC( 'A', 'T', 'I', '2' ):
 			internalFormat = GL_COMPRESSED_LUMINANCE_ALPHA_LATC2_EXT;
+			break;
+		case DDS_MAKEFOURCC( 'R', 'G', 'T', 'C' ):
+			internalFormat = GL_COMPRESSED_RED_GREEN_RGTC2_EXT;
 			break;
         default:
             common->Warning( "Invalid compressed internal format: %s", imgName.c_str() );
@@ -2160,6 +2174,9 @@ void idImage::Print() const {
 		break;
 	case GL_COMPRESSED_LUMINANCE_ALPHA_LATC2_EXT:
 		common->Printf( "LATC2 " );
+		break;
+	case GL_COMPRESSED_RED_GREEN_RGTC2_EXT:
+		common->Printf( "RGTC " );
 		break;
 	case GL_RGBA4:
 		common->Printf( "RGBA4 " );
